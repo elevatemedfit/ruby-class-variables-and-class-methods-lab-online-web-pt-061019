4b96838af6d49e2b511419d027e2ac553090212c
Chris Junker <elevatemedfit@gmail.com>
2019-07-07 19:19:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,7 @@ class Song
 attr_accessor :name,:artist,:genre
 
 @@count = 0
-@@genre = []
-@@artists = []
+
 
 def initialize
   @@count +=1
@@ -11,21 +10,5 @@ end
 
 def self.count
   @@count
-end
-
-def initialize
-  @@genre << genre
-end
-
-def self.count
-  @@genre
-
-def initialize
-  @@artists<<artist
-end
-
-def self.count
-  @@artists
-end
 end
 end
