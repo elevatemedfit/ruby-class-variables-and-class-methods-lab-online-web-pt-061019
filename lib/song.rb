8eea2bd432822999288623a88a960b5376cd6e31
Chris Junker <elevatemedfit@gmail.com>
2019-07-07 18:15:44 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+attr_accessor :name,:artist,:genre
+
+@@count = 0
+@@genre = []
+@@artists = []
+
+def initialize
+  @@count +=1
+end
+
+def self.count
+  @@count
+end
+
+def initialize
+  @@genre << genre
+end
+
+def self.count
+  @@genre
+
+def initialize
+  @@artists<<artist
+end
+
+def self.count
+  @@artists
+end
+end
+end
