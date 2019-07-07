29a5206a00fb0c4dfd187217022ce36e156f0c40
Chris Junker <elevatemedfit@gmail.com>
2019-07-07 20:42:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,10 +52,10 @@ end
 def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artists_count[artist]
-        artists_count[artist] += 1
+      if artist_count[artist]
+        artist_count[artist] += 1
       else
-        artists_count[artist] = 1
+        artist_count[artist] = 1
       end
     end
     artist_count
